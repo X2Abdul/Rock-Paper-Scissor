7bd5504f082bd60b5e7148c9709763754fb7f8b2
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-06-06 22:27:13 +0100

    all core features are done

---
+++ app.js
@@ -0,0 +1,66 @@
+const icons = document.querySelectorAll('.icon');
+const direction = document.querySelector('#direction');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document. querySelector('#computerScore');
+
+const iconsDiv = document.querySelector('.icons');
+
+function computerChoice(){
+    const choices = ['Rock', 'Paper', 'Scissor'];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+let pScore = 0;
+let cScore = 0;
+const defaultDirection = "MAKE YOUR MOVE";
+icons.forEach(icon => {
+  icon.addEventListener('click', function() {
+    //adds the css class to change apperence
+    icon.classList.add('hover-icon');
+
+        //stores computer choice
+        const c = computerChoice();
+
+        //stores players choice
+        const p = icon.id;
+
+        //compare the choices of the player and the computer
+        if(p === c){
+            direction.textContent = "You Drew";
+            setTimeout(function(){
+                direction.textContent = defaultDirection;
+            },500);
+        }else if(p === 'Rock' && c === 'Scissor' || p === 'Scissor' && c === 'Paper' || p === 'Paper' && c === 'Rock'){
+            direction.textContent = "You Win";
+            setTimeout(function(){
+                direction.textContent = defaultDirection;
+            },500);
+        
+            pScore++;
+            playerScore.textContent = pScore;
+        }else if(c === 'Rock' && p === 'Scissor' || c === 'Scissor' && p === 'Paper' || c === 'Paper' && p === 'Rock'){
+            direction.textContent = "You Lose";
+            setTimeout(function(){
+                direction.textContent = defaultDirection;
+            },500);
+            cScore++;
+            computerScore.textContent = cScore; 
+        }
+
+        //check who won
+        if(pScore === 5){
+            iconsDiv.parentNode.removeChild(iconsDiv);
+            direction.textContent = "You Win YEEEEEE (Press F5 to play agian)";            
+        }else if (cScore === 5){
+            iconsDiv.parentNode.removeChild(iconsDiv);
+            direction.textContent = "You Lose Sadge (Press F5 to play agian)";
+        }
+
+
+  });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener('transitionend', function(){
+        icon.classList.remove('hover-icon');
+      });
+});

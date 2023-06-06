c2811223b14da5173d0367b948e73eb56fac6b28
X2Abdul <87334764+X2Abdul@users.noreply.github.com>
2023-06-06 22:31:28 +0100

    nothnig much

--- app.js
+++ app.js
@@ -30,26 +30,31 @@ icons.forEach(icon => {
                 direction.textContent = defaultDirection;
             },500);
         }else if(p === 'Rock' && c === 'Scissor' || p === 'Scissor' && c === 'Paper' || p === 'Paper' && c === 'Rock'){
-            direction.textContent = "You Win";
-            setTimeout(function(){
-                direction.textContent = defaultDirection;
-            },500);
-        
+            direction.textContent = "You Won";
             pScore++;
+            if(pScore && cScore !== 5){
+                setTimeout(function(){
+                    direction.textContent = defaultDirection;
+                },500);
+            }
+        
             playerScore.textContent = pScore;
         }else if(c === 'Rock' && p === 'Scissor' || c === 'Scissor' && p === 'Paper' || c === 'Paper' && p === 'Rock'){
             direction.textContent = "You Lose";
-            setTimeout(function(){
-                direction.textContent = defaultDirection;
-            },500);
             cScore++;
+            if(pScore && cScore !== 5){
+                setTimeout(function(){
+                    direction.textContent = defaultDirection;
+                },500);
+            }
+            
             computerScore.textContent = cScore; 
         }
 
         //check who won
         if(pScore === 5){
             iconsDiv.parentNode.removeChild(iconsDiv);
-            direction.textContent = "You Win YEEEEEE (Press F5 to play agian)";            
+            direction.textContent = "You Won YEEEEEE (Press F5 to play agian)";            
         }else if (cScore === 5){
             iconsDiv.parentNode.removeChild(iconsDiv);
             direction.textContent = "You Lose Sadge (Press F5 to play agian)";
